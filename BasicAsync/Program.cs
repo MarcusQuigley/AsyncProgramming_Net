2a0c9389fc23a7e38a3b2e87bb1b2f48f761fcbf
marcusquigley <marcusquigley@gmail.com>
2014-01-14 12:30:07 -0500

    Basic AsyncProgramming with Delegates

---
+++ BasicAsync/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace BasicAsync
+{
+    class Program
+    {
+        public delegate int BinaryOp(int x, int y );
+
+        static void Main(string[] args)
+        {
+           //  SyncDelegate();
+
+            AyncDelegate();
+
+            Thread t = new Thread(new ThreadStart(MethodonThread));
+            t.
+
+            Console.Read();
+        }
+
+        static void MethodonThread()
+        { }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine("Add() invoked ON thread ID " + Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(4000);
+
+            return (x + y);
+        }
+
+        static void SyncDelegate()
+        {
+            Console.WriteLine("******* Sync Delegate review *********");
+
+            Console.WriteLine("ON thread ID " + Thread.CurrentThread.ManagedThreadId );
+
+            BinaryOp op = new BinaryOp(Add);
+
+            int result = op(10, 10);
+            Console.WriteLine("Doing more work on main");
+            Console.WriteLine(string.Format("Result of 1 + 10 ={0}", result));
+            
+        }
+
+        static void AyncDelegate()
+        {
+            Console.WriteLine("******* ASync Delegate review *********");
+
+            Console.WriteLine("ON thread ID " + Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp op = new BinaryOp(Add);
+
+              op.BeginInvoke(10, 10, AsyncCallbackMethod, null);
+             
+            Console.WriteLine("Doing more work on main");
+           
+        }
+
+        static void AsyncCallbackMethod(IAsyncResult result)
+        {
+            Console.WriteLine("ON thread ID " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Work complete");
+
+            AsyncResult ar = (AsyncResult)result;
+            BinaryOp op = (BinaryOp)ar.AsyncDelegate;
+            Console.WriteLine("result : " + op.EndInvoke(ar));
+        }
+
+
+       
+    }
+}

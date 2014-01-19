5b6fd9bad3dcb73e91b5c173f567ff41493234cc
marcusquigley <marcusquigley@gmail.com>
2014-01-19 13:08:32 -0500

    Method Groups

---
+++ SimpleThreading/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+namespace SimpleThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StoppingLoop();
+
+          //  StaticVars();
+
+            Threadpools.Program();
+
+        }
+
+        static void StoppingLoop()
+        {
+            bool stopped = false;
+
+            Thread t = new Thread(() =>
+                {
+                    while (!stopped)
+                    {
+                        Console.WriteLine("Running on {0}....", Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(700);
+                    }
+                    Console.WriteLine("Exiting thread");
+                });
+
+            t.Start();
+
+
+            Console.WriteLine("Enter key to quit and other to stop process");
+            Console.ReadKey();
+            stopped = true;
+
+            t.Join();
+            Thread.Sleep(1000);
+
+        }
+        //[ThreadStatic]
+        public static int _field;
+        public static volatile object lockObject = new object();
+        static void StaticVars()
+        {
+            new Thread(() =>
+                {
+
+
+                    while (_field < 10)
+                    {
+                        lock (lockObject)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("field {0} on threadID {1}", _field, Thread.CurrentThread.ManagedThreadId);
+                            _field++;
+                            Thread.Sleep(50);
+                        }
+                    }
+
+                }).Start();
+
+            new Thread(delegate()
+               {
+
+                   while (_field < 10)
+                   {
+                       lock (lockObject)
+                       {
+                           Console.ForegroundColor = ConsoleColor.Red;
+                           Console.WriteLine("field {0} on threadID {1}", _field, Thread.CurrentThread.ManagedThreadId);
+                           _field++;
+                           Thread.Sleep(50);
+                       }
+                   }
+
+               }).Start();
+
+            Console.WriteLine("Enter key to quit and other to stop process");
+            Console.Read();
+        }
+
+    }
+}
---
+++ SimpleThreading/Threadpools.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreading
+{
+    static class Threadpools
+    {
+        internal static void Program()
+        {
+            TasksWaitAny();
+        }
+
+        //threadpool are managed
+        internal static void MainThreadpools()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+                {
+                    Console.WriteLine("Working on threadpool");
+                });
+
+            Console.ReadLine();
+        }
+        //tasks uses threadpool
+        public static void TaskMethod()
+        {
+            //Task t = Task.Factory.StartNew(ThreadMethod);
+            //t.Wait();
+
+            Task<int> tReturn = Task<int>.Factory.StartNew(() =>
+            {
+                return 42;
+            }).ContinueWith((i) =>
+                {
+                    return (i.Result * 2);
+                });
+
+            tReturn = tReturn.ContinueWith((i) =>
+                {
+                    return (i.Result * 2);
+                });
+
+            Console.WriteLine(tReturn.Result);
+            Console.ReadLine();
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("*");
+            }
+        }
+
+        private static void TaskMethod2()
+        {
+            Task<int> t = Task<int>.Factory.StartNew(() =>
+                {
+                   // if (new Random().Next(4) == 2) throw new Exception("Exception in task 1");
+                    return 2;
+                }).ContinueWith((i) =>
+            {
+               // if (new Random().Next(4) == 2) throw new Exception("Exception in task 2");
+                return (i.Result * 2);
+            }).ContinueWith<int>((i) =>
+                {
+              //      if (new Random().Next(4) == 2) throw new Exception("Exception in task 3");
+                    return (i.Result * 2);
+                });
+            t = t.ContinueWith<int>((j) =>
+                {
+              //      if (new Random().Next(4) == 2) throw new Exception("Exception in task 4");
+                    return (j.Result * 2);   
+                });
+
+            t.ContinueWith((i) =>
+                {
+                    Console.WriteLine("Finishing up now lets return the sq of {0}", Math.Sqrt((double)i.Result));
+                    return (Math.Sqrt((double)i.Result));
+                }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            Console.WriteLine("No errors..? result={0}",t.Result);
+            Console.ReadLine();
+        }
+
+
+        private static void TasksWaitAll()
+        {
+            Task[] tasks = new Task[3];
+
+            tasks[0] = Task.Factory.StartNew(()=>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+                return 1;
+            });
+
+            tasks[1] = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 1;
+            });
+
+            tasks[2] = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 1;
+            });
+
+            Task.WaitAll(tasks);
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+
+        private static void TasksWaitAny()
+        {
+            Task<int>[] tasks = new Task<int>[3];
+            tasks[0] = Task.Factory.StartNew(()=>
+                {
+                    Console.WriteLine("Task 1 is working");
+                    Thread.Sleep(4000);
+                    return 1;
+                });
+
+            tasks[1] = Task.Factory.StartNew<int>(() =>
+            {
+                Console.WriteLine("Task 2 is working");
+                Thread.Sleep(1000);
+                return 2;
+            });
+
+            tasks[2] = Task.Factory.StartNew<int>(() =>
+            {
+                Console.WriteLine("Task 3 is working");
+                Thread.Sleep(2000);
+                return 3;
+            });
+
+
+
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask =tasks[i];
+
+                Console.WriteLine("Task has finished. Result={0}", completedTask.Result);
+                var tempList = tasks.ToList();
+                tempList.RemoveAt(i);
+                tasks = tempList.ToArray();
+           }
+
+            Console.WriteLine("All tasks completed");
+                Console.ReadLine();
+        }
+    }
+}
